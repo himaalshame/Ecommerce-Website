77dd81c1f876d76dfd05368e472c000fe814c5cb
Ibrahim Ilshamy <129802781+himaalshame@users.noreply.github.com>
2026-04-09 21:33:21 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import CartProvider from './components/context/CartContext.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <HashRouter>
       <CartProvider>
         <App />
       </CartProvider>
     </HashRouter>
-  </React.StrictMode>,
+  </StrictMode>,
 )
